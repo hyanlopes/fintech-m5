1ccf7aeb337ba7b6979991edcb0a342a88df0b97
Richard Cândido <rich_dacan@protonmail.com>
2022-09-06 13:39:19 -0300

    feat :sparkles: created serializer

--- wallets/models.py
+++ wallets/models.py
@@ -6,6 +6,13 @@ from django.db import models
 class Wallet(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=50)
-    wallet_id = models.ManyToManyField(
-        "financial_assets.FinancialAssets", related_name="financial_assets"
+
+    # FK 1:N - users
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="users"
     )
+
+    # FK N:N - financial assets
+    # financial_assets = models.ManyToManyField(
+    #     "financial_assets.FinancialAssets", related_name="financial_assets"
+    # )
