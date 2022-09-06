e5e2849c005fb9169aac9466fdf2050d5b81ae22
Richard Cândido <rich_dacan@protonmail.com>
2022-09-06 14:24:04 -0300

    feat: created urls and views

--- fintech_m5/urls.py
+++ fintech_m5/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("users.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("users.urls")),
+    path("api/", include("wallets.urls")),
+]
--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from wallets.models import Wallet
+from wallets.serializers import WalletSerializer
+
+
+class WalletView(generics.ListCreateAPIView):
+    serializer_class = WalletSerializer
+    queryset = Wallet.objects.all()
