7dbac6a998930d1b3bc37c1551a5a3cdb2c84f8e
Tarcila Garcia <tarcilasg@gmail.com>
2022-09-06 10:45:22 -0300

    feat: Financial Assets model created

--- financial_assets/models.py
+++ financial_assets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class FinancialAssets(models.Models):
+    name = models.CharField(max_length=50)
+    transaction_type = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_date_time = models.DateTimeField(auto_now=True)
