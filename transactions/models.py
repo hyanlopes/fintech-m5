ab3d209d38166ddbe1f9baf37ca9fddce735dd0b
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-09 17:31:16 -0300

    feat :sparkles: fixed the structor of apps

--- transactions/models.py
+++ transactions/models.py
@@ -8,6 +8,7 @@ class Transaction(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False, auto_created=True
     )
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    # total_price?
     quantity = models.DecimalField(max_digits=10, decimal_places=10)
     type = models.CharField(max_length=256, default="-")
 
