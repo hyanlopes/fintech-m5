09af10d1395146545d00dd2d3033b76e3ea4fe42
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-09 17:09:41 -0300

    feat :sparkles: created transactions and wallet relationship

--- wallets/views.py
+++ wallets/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
-from extracts.models import Extract
-from extracts.serializers import ExtractSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from wallets.models import Wallet
 from wallets.serializers import WalletSerializer
 
 
 class WalletView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = WalletSerializer
     queryset = Wallet.objects.all()
 
-
-
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
