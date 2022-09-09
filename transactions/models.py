006a47c1b6cd0a5fd6e914df73b9e8257c49452a
Hyan Lopes <hyanlopess97@gmail.com>
2022-09-09 15:27:36 -0300

    feat: transactions app made

--- transactions/models.py
+++ transactions/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 
 
-class Extract(models.Model):
+class Transaction(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, auto_created=True
     )
@@ -11,6 +11,9 @@ class Extract(models.Model):
     quantity = models.DecimalField(max_digits=10, decimal_places=10)
     type = models.CharField(max_length=256, default="-")
 
-    # wallets = models.ForeignKey(
-    #     "wallets.Wallet", on_delete=models.CASCADE, related_name="extracts"
-    # )
+    wallets = models.ForeignKey(
+        "wallets.Wallet", on_delete=models.CASCADE, related_name="transactions"
+    )
+
+
+# Create your models here.
