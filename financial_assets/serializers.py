394b5b163b37b87761879cb8a5767714117611f1
Tarcila Garcia <tarcilasg@gmail.com>
2022-09-11 18:33:52 -0300

    added real price from integration api

--- financial_assets/serializers.py
+++ financial_assets/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 
+from .api_data import DataCrypto
 from .models import Asset
 
 #from wallets.serializers import WalletSerializer
- 
- 
- 
+
+
+
 class AssetSerializer(serializers.ModelSerializer):
     class Meta:
        model = Asset
        fields = "__all__"
-    
+    real_price = serializers.SerializerMethodField()
 
+    def get_real_price(self, obj):
+        return DataCrypto.get()["price_actual"]
